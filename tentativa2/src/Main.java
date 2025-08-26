227e5e36ecfa87c087060d42f1e008a6105eda5e
Vitor Roberto Silverio Martins <vitorrsilveriomartins@gmail.com>
2025-08-26 15:30:06 -0300

    Update Main.java

--- tentativa2/src/Main.java
+++ tentativa2/src/Main.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
             public static void main(String[] args) {
                 File file = new File("C:\\temp\\in.txt");
